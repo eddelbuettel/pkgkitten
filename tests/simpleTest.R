e58ce09bc78433793e700614c70ecac72a1ca305
Dirk Eddelbuettel <edd@debian.org>
2014-06-12 17:09:09 -0500

    unset R_TESTS so that R CMD build and check can run
    with thanks to Hadley for the tip

--- tests/simpleTest.R
+++ tests/simpleTest.R
@@ -5,10 +5,12 @@ setwd(tempdir())                        # which gets deleted anyway
 
 kitten("simpleTest")                    # unfortunately testing fails :-/
 
-## R <- shQuote(file.path(R.home(component="bin"), "R"))
-## #system(paste(R, "--version"))
-## #system(paste(R,  "CMD build", pkgpath))
-## #system(paste(R,  "CMD check --no-manual --no-vignettes simpleTest_1.0.tar.gz"))
+R <- shQuote(file.path(R.home(component="bin"), "R"))
+
+Sys.setenv("R_TESTS"="")                # thanks for the tip, Hadley
+system(paste(R, "CMD build simpleTest"))
+
+system(paste(R, "CMD check --no-manual --no-build-vignettes simpleTest_1.0.tar.gz"))
 
 
 
