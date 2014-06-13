129124d72f0d029d63173e15f6cb5c3d20392872
Dirk Eddelbuettel <edd@debian.org>
2014-06-12 20:26:26 -0500

    move tests/simpleTest.R to demo/simpleDemo.R

--- demo/simpleDemo.R
+++ demo/simpleDemo.R
@@ -7,10 +7,10 @@ kitten("simpleTest")                    # unfortunately testing fails :-/
 
 R <- shQuote(file.path(R.home(component="bin"), "R"))
 
-Sys.setenv("R_TESTS"="")                # thanks for the tip, Hadley
+Sys.setenv("R_TESTS"="")                # needed for R CMD check; thanks for the tip, Hadley
 system(paste(R, "CMD build simpleTest"))
 
-system(paste(R, "CMD check --no-manual --no-build-vignettes simpleTest_1.0.tar.gz"))
+system(paste(R, "CMD check simpleTest_1.0.tar.gz"))
 
 
 
